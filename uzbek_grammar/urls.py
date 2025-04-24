6dfa9610744053b481909e34bd3d6beac0b8fcf1
Sanjarbek1401 <159608407+Sanjarbek1401@users.noreply.github.com>
2025-04-24 14:29:44 +0500

    Update urls.py

--- uzbek_grammar/urls.py
+++ uzbek_grammar/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('grammar_app.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 # Serve media files in development
 if settings.DEBUG:
